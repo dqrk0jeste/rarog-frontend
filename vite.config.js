3c32450f3a96ae35768c55eb4a459ff6a957b1e7
dqrk0jeste <darkonikoloc@gmail.com>
2024-05-02 19:14:07 +0200

    fuck ,e

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,6 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         home: resolve(__dirname, 'home/index.html'),
         signup: resolve(__dirname, 'signup/index.html'),
-        books: resolve(__dirname, 'books/index.html'),
-        singleBooks: resolve(__dirname, 'books/single/index.html'),
         music: resolve(__dirname, 'music/index.html'),
         singleMusic: resolve(__dirname, 'music/single/index.html'),
         movie: resolve(__dirname, 'movies/index.html'),
